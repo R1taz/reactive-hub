3a01e6c620ceaa1ce1a2223f0bbbd3a1e525ef3b
Ritaz <Chichurin06@mail.ru>
2025-02-05 18:34:33 +0300

    create newsPage and infinite scroll

---
+++ src/api/newsAPI.ts
@@ -0,0 +1,23 @@
+import {
+	RequestGetApiNews,
+	ResponseGetApiNews,
+} from '@/interfaces/newsInterface'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const newsAPI = createApi({
+	reducerPath: 'newsAPI',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'https://api.currentsapi.services/v1/search',
+	}),
+	endpoints: builder => ({
+		getNews: builder.query<ResponseGetApiNews, RequestGetApiNews>({
+			query: ({ page_number, page_size }) =>
+				`?apiKey=${
+					import.meta.env.VITE_NEWS_API_KEY
+				}&page_number=${page_number}&page_size=${page_size}`,
+		}),
+	}),
+})
+
+export default newsAPI
+export const { useGetNewsQuery } = newsAPI
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
 import gamesSlice from './slices/gamesSlice'
 import profileSlice from './slices/profileSlice'
+import newsSlice from './slices/newsSlice'
 import gamesAPI from '@/api/gamesAPI'
+import newsAPI from '@/api/newsAPI'
 
 const store = configureStore({
 	reducer: {
 		gamesSlice,
 		profileSlice,
+		newsSlice,
 		[gamesAPI.reducerPath]: gamesAPI.reducer,
+		[newsAPI.reducerPath]: newsAPI.reducer,
 	},
 	middleware: getDefaultMiddleware =>
-		getDefaultMiddleware().concat(gamesAPI.middleware),
+		getDefaultMiddleware().concat([gamesAPI.middleware, newsAPI.middleware]),
 })
 
 export type RootState = ReturnType<typeof store.getState>
